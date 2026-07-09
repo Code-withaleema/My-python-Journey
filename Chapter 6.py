a3669877389a6993b015bf1458867f649c62763d
Code-withaleema <aleemabaig236@gmail.com>
2026-07-09 22:23:49 +0500

    Add files via upload

---
+++ Chapter 6.py
@@ -0,0 +1,84 @@
+# # #Project:1
+# countries=("Iran","Turkey","Pakistan","Edinbrugh","Germeny")
+# print(countries[1])
+# print(countries[-1])
+# a=input("Enter country you want to search: ")
+# if a in countries:
+#     print("Found")
+# else:
+#     print("Not Found")
+# print("Total countries: ",len(countries))
+# #Project:2
+# numbers=(10,20,10,30,10)
+# print(numbers.count(10))
+# #Project:3
+# n=(15,25,35,45,55)
+# print("Maximum: ",max(n))
+# print("Minimum: ",min(n))
+# print("Sum: ",sum(n))
+#Project:4
+# fruits=("Apple","Mango","Orange","Banana")
+# if "Apple" in fruits:
+#     print("Found")
+# else:
+#     print("Not Found")
+# Project:5
+# student=("Ali",20,"Bscs",3.8)
+# name,age,degree,cgpa=student
+# print("Name: ",name)
+# print("Age: ",age)
+# print("Degree: ",degree)
+# print("CGPA: ",cgpa)
+# #Project:6
+# employee=(101,"Ahmed","Manager",80000)
+# EmployeeID,name,designation,salary=employee
+# print("Employee ID: ",EmployeeID)
+# print("Name: ",name)
+# print("Designation: ",designation)
+# print("Salary: ",salary)
+#Project:7
+# subjects=("Math","Physics","English","Computer","Urdu")
+# print("All subjects: ",subjects)
+# print("Index of computer: ",subjects.index("Computer"))
+# if "Math" in subjects:
+#     print("Math exists")
+# else:
+#     print("Math not exists")
+# print("Total Subjects: ",len(subjects))
+# #Project:8
+# temps=(32,35,30,28,40,36,33)
+# print("Highest temperature: ",max(temps))
+# print("Lowest temperature: ",min(temps))
+# print("Average temperature: ",sum(temps)/len(temps))
+# print("Total temperature: ",sum(temps))
+# print("NUmber of Readings: ",len(temps))
+# #Project:9
+# point=(10,20)
+# print("X Coordinate: ",point[0])
+# print("Y Coordinate: ",point[1])
+#Project:9
+# product=("Laptop",85000,"HP","Silver")
+# productname,price,brand,color=product
+# print("Product Name: ",productname)
+# print("Price: ",price)
+# print("Brand: ",brand)
+# print("Color: ",color)
+# #Project:10
+# days=("Monday","Tuesday","Wednesday","Thursday","Friday")
+# print("Days: ",days)
+# print("First Day: ",days[0])
+# print("Last Day: ",days[-1])
+# print("Index of Wednesday: ",days.index("Wednesday"))
+# if "Sunday"in days:
+#     print("Sunday exists")
+# else:
+#     print("Sunday not exists")
+#Challenge project
+student=("Ali",85,90,78,88,95)
+print("Student Name: ",student[0])
+marks=student[1:]
+print("Total marks: ",sum(marks))
+print("average marks: ",sum(marks)/len(marks))
+print("Highest Marks: ",max(marks))
+print("Lowest Marks: ",min(marks))
+
