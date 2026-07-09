2e1f129eefcc397b37e59dba7cb3bf026835a691
Code-withaleema <aleemabaig236@gmail.com>
2026-07-09 22:22:01 +0500

    Add files via upload

---
+++ Chapter1.py
@@ -0,0 +1,74 @@
+#Practice task 1
+name=input("Enter name : ")
+age=input("Enter age : ")
+print("My name is ",name)
+print("I am",age,"years old")
+#Practice task 2
+city=input("Enter City : ")
+country=input("Enter Country : ")
+university=input("Enter University : ")
+print("City : ",city)
+print("Country : ",country)
+print("University : ",university)
+#Practice task 3
+num1=15
+num2=10
+print("Sum : ",num1+num2)
+print("Difference : ",num1-num2)
+print("Product : ",num1*num2)
+print("Division : ",num1/num2)
+#Practice task 4
+is_student=True
+print(is_student)
+#Practice task 5
+n1=int(input("Enter First Number : "))
+n2=int(input("Enter Second Number : "))
+print("Sum : ", n1+n2)
+#Practice task 5
+Grade="A+"
+Marks=89
+Gpa=3.6
+Answer=True
+print(type(Grade))
+print(type(Marks))
+print(type(Gpa))
+print(type(Answer))
+#Practice task 6
+a=10
+b=5
+print("Before")
+print("a : ",a)
+print("b : ",b)
+
+temp=a
+a=b
+b=temp
+print("After")
+print("a : ",a)
+print("b : ",b)
+#Practice task 7
+sq=int(input("Enter number : "))
+print("Input : ",sq)
+print("Output : ",sq*sq)
+#Practice task 8
+length=int(input("Enter length : "))
+width=int(input("Enter width : "))
+print("Length :",length)
+print("Width : ",width)
+print("Area : ",length*width)
+#Mini Project 1
+Name=input("Enter name : ")
+Age=int(input("Enter age : "))
+Degree=input("Enter Degree : ")
+Semester=int(input("Enter Semester : "))
+University=input("Enter University : ")
+print("-----Student Profile-----")
+print("Name : ",Name)
+print("Age : ",Age)
+print("Degree : ",Degree)
+print("Semester : ",Semester)
+print("University : ",University)
+print("--------------------------")
+
+
+
