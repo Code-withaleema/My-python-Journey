2b18294256522f993a8ded4158d741eb5f0ea298
Code-withaleema <aleemabaig236@gmail.com>
2026-07-09 22:22:30 +0500

    Add files via upload

---
+++ Chapter2.py
@@ -0,0 +1,35 @@
+#Practice : 1
+num=int(input("Enter Number : "))
+if(num%2==0):
+    print("Even")
+else:
+    print("Odd")
+#Practice : 2
+age=int(input("Enter Age : "))
+if(age >= 18):
+    print("Eligible to vote")
+else:
+    print("Not eligible")
+#Practice : 3
+marks=int(input("Enter Number : "))
+if(marks >= 80):
+    print("A")
+elif(marks >= 60):
+    print("B")
+elif(marks >= 40):
+    print("C")
+else:
+    print("Fail")
+#Practice : 4
+n=int(input("Enter number : "))
+if(n%5==0):
+    print("Divisible by 5")
+else:
+    print("Not Divisible by 5")
+#Practice : 5
+a=input("Enter Username : ")
+b=input("Enter Password : ")
+if(a=="admin" and b=="1234"):
+    print("Login Successful")
+else:
+    print("Invalid Credentials")   
